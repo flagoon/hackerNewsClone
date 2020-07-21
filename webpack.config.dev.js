d7ab98a773456ffc84f8379834090ed7e084c346
Pawel Kochanek <pawel.kochanek@avid.com>
2020-07-21 10:08:15 +0200

    config for everything

---
+++ webpack.config.dev.js
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTsCheckerNotifierWebpackPlugin = require('fork-ts-checker-notifier-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const webpackBase = require('./webpack.config.base');
+
+module.exports = merge(webpackBase, {
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundles.js',
+  },
+  plugins: [
+    // this plugin is checking and reporting problems with typescript types. Without it,
+    // webpack will not report errors, if compilation is correct. If we don't use {transpileOnly: true}
+    // webpack will check types differently and therefore would take more time.
+    new ForkTsCheckerWebpackPlugin({
+      eslint: {
+        files: './src/**/*.{ts,tsx,js,jsx}',
+      },
+    }),
+    // show system notification while typescript has checked types correctly or not.
+    new ForkTsCheckerNotifierWebpackPlugin({
+      title: 'TypeScript',
+      alwaysNotify: true,
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'src/index.html',
+    }),
+  ],
+  devtool: 'source-map',
+  devServer: {
+    clientLogLevel: 'warning',
+    open: true,
+    port: 3000,
+    historyApiFallback: true,
+    stats: 'errors-only',
+  },
+});
---
+++ webpack.config.prod.js
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require('path');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const webpackBase = require('./webpack.config.base');
+
+module.exports = merge(webpackBase, {
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'app.bundles.js',
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      typescript: {
+        enabled: true,
+        memoryLimit: 4096,
+      },
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+      reportFilename: 'bundle_sizes.html',
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      inject: true,
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    }),
+  ],
+
+  externals: { react: 'React', 'react-dom': 'ReactDOM' },
+});
