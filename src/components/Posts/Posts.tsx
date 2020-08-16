2bf69b0e9b3eda30521841b210e940bbdd4161c8
Pawel Kochanek <pawel.kochanek@avid.com>
2020-08-16 12:15:44 +0200

    7: detecting bottom page

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import debounce from 'lodash/debounce'
 import { getPosts } from '../../api'
 import Message from '../Message/Message'
 import Loading from '../Loading/Loading'
@@ -11,10 +12,34 @@ const Posts: React.FC<{ ids: number[]; type: 'story' | 'comment' }> = ({
   ids,
   type,
 }) => {
+  const containerRef = React.useRef<HTMLDivElement>(null)
+
   const [state, dispatch] = React.useReducer(postsReducer, {
     isLoading: false,
     posts: [],
   })
+
+  const fetchMoreStories = () => {
+    const postsDivHeight = containerRef.current?.clientHeight
+    // size of whole window, from top scroll position to bottom scroll position.
+    const windowWholeArea = document.documentElement.scrollHeight
+    const windowVisibleArea = window.innerHeight
+    const windowTopScrollPosition = window.scrollY
+    if (windowTopScrollPosition + windowVisibleArea > windowWholeArea - 500) {
+      console.warn('scrollHeight: ', postsDivHeight)
+      console.warn('window: ', windowWholeArea)
+      console.error('windowVisibleArea: ', windowVisibleArea)
+      console.error('windowTopScrollPosition: ', windowTopScrollPosition)
+    }
+  }
+
+  const fetchWithScroll = debounce(fetchMoreStories, 300)
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', fetchWithScroll)
+    return () => window.removeEventListener('scroll', fetchWithScroll)
+  }, [fetchWithScroll])
+
   React.useEffect(() => {
     if (ids.length > 0) {
       dispatch(fetchPosts())
@@ -36,7 +61,7 @@ const Posts: React.FC<{ ids: number[]; type: 'story' | 'comment' }> = ({
   }
 
   return (
-    <>
+    <div ref={containerRef}>
       {state.posts.map((post) =>
         type === 'story' ? (
           <Message key={post.id} message={post} />
@@ -44,7 +69,7 @@ const Posts: React.FC<{ ids: number[]; type: 'story' | 'comment' }> = ({
           <Comment comment={post} />
         ),
       )}
-    </>
+    </div>
   )
 }
 
